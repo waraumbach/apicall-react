2a6de8f8c76ec8bed56ebbafad29a9f3e015f889
wara <144021938+waraumbach@users.noreply.github.com>
2024-02-26 15:05:57 +0100

    updated

--- @latest/src/user.jsx
+++ @latest/src/user.jsx
@@ -3,15 +3,19 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const User = () => {
-  const apiCall = () => {
+  const apiCall = async () => {
     try {
-      const response = await;
-      axios.get("https://jsonplaceholder.typicode.com/photos");
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/photos"
+      );
       console.log(response);
     } catch (err) {
       console.log(err);
     }
   };
+  useEffect(() => {
+    apiCall;
+  });
 
   return;
   <></>;
